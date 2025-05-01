4e511a711108daf935452c086a5e73aa69ac3383
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-05-01 10:51:09 -0300

    2 Commit

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+def pegarNumeros():
+    num1 = int(input("Insira o primeiro número: "))
+    num2 = int(input("Insira o segundo número: "))
+    return num1, num2
+
+def Calculos(num1, num2):
+    print("Subtração:",num1 - num2)
+    print("Soma:",num1+num2)
+    print("Multiplicação",num1*num2)
+
+    if num2 == 0:
+        print("Divisão: [ERRO] Não é possível dividir por 0")
+    else:
+        print("Divisão:",num1/num2)
+
